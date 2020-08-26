e277e9f60a1ccc3517f4d1ef1d11ce3ebc9a32e5
Sharon Walters <SHARONMARIE919@GMAIL.COM>
2020-08-26 16:36:25 -0400

    I'M EXCITED

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 import logo from "./images/logo.svg";
 import "./App.css";
-
+const todoList ={
+  task: "Laundry",
+  task2:"Feed the children",
+  task3:"Bake cookies for class",
+  task4:"DRINK"
+};
+let taskStatus =[
+  {
+completed: false,
+reoccuring: false
+}
+];
 function App() {
   return (
     <>
@@ -13,7 +24,15 @@ function App() {
       <input type="text" />
       <button>Save</button>
       <button>Save & View List</button>
-
+      <ol>
+       <li><input type="checkbox" checked={todoList.completed}/>{todoList.task}</li>
+       
+       
+        <li><input type="checkbox"/>{todoList.task2}</li>
+        <li><input type="checkbox"/>{todoList.task3}</li>
+        <li><input type="checkbox"/>{todoList.task4}</li>
+      </ol>
+      <button>Save</button>
     </>
   );
 }
