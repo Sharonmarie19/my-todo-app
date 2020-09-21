b3cba3ecb57af03be3471db3e6a864cc649cefd5
Sharon Walters <SHARONMARIE919@GMAIL.COM>
2020-09-21 14:46:25 -0400

    clock added

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,24 +1,34 @@
 import React from "react";
 
-class Clock extends React.Component{
-    state= {
-        now: new Date(),
-    };
-    tick = () => {
-        this.setState({ now: new Date() });
-    };
-    componentDidMount(){
-        setInterval(()=> this.tick(), 1000);
+class Clock extends React.Component {
+  state = {
+    now: new Date(),
+  };
+  tick = () => {
+    this.setState({ now: new Date() });
+  };
+  componentDidMount() {
+    setInterval(() => this.tick(), 1000);
+    this.setState((state) => {
+      return { day: state.now.getDay() };
+    });
+  }
+  render() {
+    let greeting = "";
+    if (this.state.day == 2) {
+      greeting = "Happy Tuesday!";
     }
-    render(){
-        return <div>{this.state.now.toLocaleTimeString()}</div>;
+    if (this.state.day === 5) {
+      greeting = "TGIF!!!";
+    }else if (this.state.day === 0){
+        greeting = "What day is it?";
     }
+    return (
+      <div>
+        {greeting}
+        {this.state.now.toLocaleTimeString()}
+      </div>
+    );
+  }
 }
-
-
-
-
-
-
-
 export default Clock;
